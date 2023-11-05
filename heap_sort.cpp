6e369b41109431ae6bc11c2cc419549b0bfa099e
meiyuu0305 <121405997+meiyuu0305@users.noreply.github.com>
2023-11-04 23:05:40 -0500

    Create heap_sort.cpp
    heap_sort, with count for steps, and generate random vector

---
+++ heap_sort.cpp
@@ -0,0 +1,89 @@
+#include <iostream> 
+#include <vector>
+using namespace std;
+
+
+unsigned get_rand_in_range(unsigned low, unsigned high);
+
+void printArray(vector <int> ptr, int size, int begin = 0){
+    for (int i = begin; i < size; i++){
+        cout << ptr[i] << " ";
+    }
+    cout << endl;
+};
+void buildHeap(vector <int> &ptr, int size, int hole, int &count){
+    int root = hole;
+    while((2*root+2 < size)){
+    int left = 2*root+1;
+    int right = left+1;
+    int change_index = root;
+    if(ptr[left] > ptr[root] && ptr[left] > ptr[right]){
+        change_index = left;
+    }
+    else if(ptr[right] > ptr[root] && ptr[right] > ptr[left]){
+        change_index = right;
+    }
+    count++; // comparing elements
+    if(change_index != root){
+        int temp = ptr[root];
+        ptr[root] = ptr[change_index];
+        ptr[change_index] = temp;
+        root = change_index;
+        count++; //swap array element
+    }
+    root = left;
+    }
+};
+
+int heap_sort(vector<int> & ptr, int size){
+    int count = 0; 
+    buildHeap(ptr,size,0, count);
+    for(int k = size-1; k >= 0; k--){
+        int temp = ptr[0];
+        ptr[0] = ptr[k];
+        ptr[k] = temp;
+        buildHeap(ptr,k,0, count);
+    }
+    return count;
+};
+int main() {
+    srand(time(0)); // in order to generate random numbers
+    const int VECTOR_SIZE = 50000;
+    vector<int> test_values;
+    double sumOfComparison = 0;
+    int comp_ran=0, comp_asc=0, comp_desc=0;
+//    for(int i=0;i<SIMUL_SIZE; i++)
+//     { 
+    // clearing the vector, so, we can start with an empty vector
+    test_values.clear();
+    // generating 1000 random values and storing them to the vector
+    for(int k=0;k<VECTOR_SIZE;k++)
+    {
+      int value = get_rand_in_range(1, VECTOR_SIZE);
+      test_values.push_back(value); 
+    }
+    vector<int> asc_test_values = test_values;
+    vector<int> desc_test_values = test_values;
+    sort(asc_test_values.begin(), asc_test_values.end());
+    sort(desc_test_values.begin(), desc_test_values.end(), greater<int>());
+
+    comp_ran = heap_sort(test_values, VECTOR_SIZE); 
+    comp_asc = heap_sort(asc_test_values, VECTOR_SIZE); 
+    comp_desc = heap_sort(desc_test_values, VECTOR_SIZE); 
+    //}
+    sumOfComparison = comp_ran + comp_asc + comp_desc;
+    cout<<"\nNumber of comparisons in random vector: " <<comp_ran<<endl; // /simul_size
+    cout<<"\nNumber of comparisons in ascending vector: " <<comp_asc<<endl;
+    cout<<"\nNumber of comparisons in descending vector: " <<comp_desc<<endl;
+    cout<<"\nAverage number of comparisons: " <<sumOfComparison/3<<endl;
+    return 0;
+}
+unsigned get_rand_in_range(unsigned low, unsigned high)
+{
+  int random_number=0;
+  // generate the random number
+  random_number = low + rand() % (high - low + 1);
+  return random_number;
+}
+
+
